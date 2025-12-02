6cdc86d4a6069fe371ca31f1fc76a2afd7438326
Zuppah1 <mantvydas.rinkunas@gmail.com>
2025-12-02 17:23:04 +0200

    Config json struct, read, write funcs

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main()
+import (
+	"fmt"
+	"log"
+
+	"github.com/Zuppah1/Blog-Aggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("mantvydas")
+	if err != nil {
+		log.Fatalf("could not set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+
+	fmt.Printf("Read config again: %+v\n", cfg)
+}
